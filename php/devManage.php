4b4f15c12305fa283df733d1abe041d5b3f588e4
root <root@localhost.localdomain>
2015-08-05 12:01:22 -0400

    2

--- php/devManage.php
+++ php/devManage.php
@@ -20,8 +20,15 @@ class devManage{
 	//创建一个设备
 	public function createAccount(){
 		$con = self::connectMysql();
-		mysql_select_db("my_db", $con);
-		$qurry = ""
+		mysql_select_db("deviceSYS", $con);
+		$querry = "INSERT INTO devices (device_name,device_mode,pixel,ram,screen_size,cpu_hz,for_camara,back_camara,color,numbers,sim_number,sdcard) VALUES ('三星','Glax2','1024*768','1','5.0','2.2','200','500','黑色','1','18611513354','8')";		
+		mysql_query("SET NAMES 'UTF8'");  
+		mysql_query("SET CHARACTER SET UTF8");  
+		mysql_query("SET CHARACTER_SET_RESULTS=UTF8'"); 
+		mysql_query($querry);
+
+		//mysql_query("INSERT INTO devices (device_name,device_mode,pixel,ram,screen_size,cpu_hz,for_camara,back_camara,color,numbers,sim_number,sdcard) VALUES ('三星','Glax2','1024*768','1','5.0','2.2','200','500','黑色','1','18611513354','4')");2015/8/5
+		mysql_close($con);
 		echo $con;
 	}
 
