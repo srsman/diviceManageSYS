246519ab1536bc7971461e7c2bb8ef6e7e742514
root <root@localhost.localdomain>
2015-08-06 06:53:46 -0400

    3

--- php/devManage.php
+++ php/devManage.php
@@ -17,33 +17,44 @@ class devManage{
 		}
 	}
 
-	//创建一个设备
+	//向数据库添加一个设备
 	public function createAccount(){
 		$con = self::connectMysql();
 		mysql_select_db("deviceSYS", $con);
 		$querry = "INSERT INTO devices (device_name,device_mode,pixel,ram,screen_size,cpu_hz,for_camara,back_camara,color,numbers,sim_number,sdcard) VALUES ('三星','Glax2','1024*768','1','5.0','2.2','200','500','黑色','1','18611513354','8')";		
-		mysql_query("SET NAMES 'UTF8'");  
-		mysql_query("SET CHARACTER SET UTF8");  
-		mysql_query("SET CHARACTER_SET_RESULTS=UTF8'"); 
+		self::setCoding();
 		mysql_query($querry);
-
-		//mysql_query("INSERT INTO devices (device_name,device_mode,pixel,ram,screen_size,cpu_hz,for_camara,back_camara,color,numbers,sim_number,sdcard) VALUES ('三星','Glax2','1024*768','1','5.0','2.2','200','500','黑色','1','18611513354','4')");2015/8/5
 		mysql_close($con);
 		echo $con;
 	}
 
-	public function test(){
-		echo "happy<br/>";
-		$aaa = new Config;
-		echo $aaa->username;
+	//查询数据库中所有数据
+	public function getAllData(){
+		$con = self::connectMysql();
+		mysql_select_db("deviceSYS", $con);
+		$querry = "SELECT * FROM devices";
+		self::setCoding();
+		$result = mysql_query($querry);
+		$arr = array();
+		while($row = mysql_fetch_array($result)){
+			//echo var_dump($row);
+			$arr[] = $row;
+			//echo json_encode($row);
+			//echo "<br/>";
+		}
+		echo json_encode($arr);
+		//echo $result;
 	}
 
-	public function test1(){
-		echo "test1";
+	//设置访问数据库编码
+	public function setCoding(){
+		mysql_query("SET NAMES 'UTF8'");  
+		mysql_query("SET CHARACTER SET UTF8");  
+		mysql_query("SET CHARACTER_SET_RESULTS=UTF8'"); 
 	}
 }
 
 	$obj = new devManage;
-	$obj->createAccount();
+	$obj->getAllData();
 ?>
 
