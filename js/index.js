4992dcfe7fbf38604470c74453f9a884562cc551
root <root@localhost.localdomain>
2015-08-14 06:38:04 -0400

    7

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//查询设备和设备管理界面切换
 function divSwitch(e){
 	e = e || window.event;
 	if(e.target.id == "label_search"){
@@ -16,6 +17,7 @@ function divSwitch(e){
 	}
 }
 
+//弹出登陆界面或注册页面
 function divLogin(e){
 	e = e||window.event;
 	if (e.target.id == "label_login"){
@@ -29,6 +31,7 @@ function divLogin(e){
 	}
 }
 
+//关闭登陆界面
 function closeLogin(e){
 	e = e||window.event;
 	$("#main").css("display","block");
@@ -36,9 +39,28 @@ function closeLogin(e){
 	$("#registe_panel").css("display","none");
 }
 
+//关闭注册界面
 function closeRegister(e){
 	e = e||window.event;
 	$("#main").css("display","block");
 	$("#login_panel").css("display","none");
 	$("#registe_panel").css("display","none");
+}
+
+//添加一个设备
+function addDevice(e){
+	e = e||window.event;
+	var device_name = $("device_name").val();
+	var device_mode = $("device_mode").val();
+	var pixel = $("pixel").val();
+	var ram = $("ram").val();
+	var cpu_hz = $("cpu_hz").val();
+	var screen_size = $("screen_size").val();
+	var color = $("color").val();
+	var for_camara = $("for_camara").val();
+	var back_camara = $("back_camara").val();
+	var sim_number = $("sim_number").val();
+	var sdcard = $("sdcard").val();
+
+	alert(device_name + device_mode + pixel);
 }
